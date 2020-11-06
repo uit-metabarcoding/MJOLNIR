367df82cccf4aeeea365f9c07c9d9f3de32044aa
UiT Metabarcoding <70535159+uit-metabarcoding@users.noreply.github.com>
2020-11-06 20:50:04 +0100

    Update mjolnir_lulu.R

--- R/mjolnir_lulu.R
+++ R/mjolnir_lulu.R
@@ -18,7 +18,7 @@ mjolnir_lulu <- function(lib,min_id = .84){
   system(paste0("vsearch --usearch_global ",lib,".seeds_nonsingleton.fasta --db ",lib,".seeds_nonsingleton.fasta --self --id ",min_id," --iddef 1 --userout ",lib,".match_list.txt -userfields query+target+id --maxaccepts 0 --query_cov .9 --maxhits 10"),intern=T,wait=T)
   message("Removing pseudogenes with LULU.")
   
-  library(lulu)
+  suppressPackageStartupMessages(library(lulu))
   
   #Load the dataset
   db <- read.csv(paste0(lib,".All_MOTUs.csv"),sep=";",head=T,stringsAsFactors = F)
