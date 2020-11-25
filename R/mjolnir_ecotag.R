fe5de9246cb6d4865287910556a2cec88cf4dbb5
UiT Metabarcoding <70535159+uit-metabarcoding@users.noreply.github.com>
2020-11-25 21:49:58 +0100

    Update mjolnir_ecotag.R

--- R/mjolnir_ecotag.R
+++ R/mjolnir_ecotag.R
@@ -1,4 +1,4 @@
-mjolnir_ecotag <- function(lib,cores,tax_dir,ref_db,taxo_db){
+mjolnir_THOR <- function(lib,cores,tax_dir,ref_db,taxo_db){
   message("Splitting seeds file in ",cores," fragments.")
   system(paste0("obidistribute -n ",cores," -p ",lib,".seeds ",lib,".seeds_nonsingleton.fasta"),intern=T,wait=T)
   message("Taxonomic assignment with ecotag")
@@ -13,6 +13,4 @@ mjolnir_ecotag <- function(lib,cores,tax_dir,ref_db,taxo_db){
   message("Adding higher taxonomy ranks.")
   system(paste0("cat ",lib,".seeds.ecotag_??.fasta > ",lib,".ecotag.fasta"),intern=T,wait=T)
   owi_add_taxonomy(paste0(lib,".ecotag.fasta"),tax_dir)
-  message("Producing the combined file.")
-  owi_combine(infile=paste0(lib,".ecotag.fasta.annotated.csv"),abundances=paste0(lib,".SWARM_output.counts.csv"),outfile=paste0(lib,".All_MOTUs.csv"))
-}
+  }
