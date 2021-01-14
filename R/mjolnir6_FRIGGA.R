0e8ca4c094e569612a516258b4ac5f4a16b5eb47
UiT Metabarcoding <70535159+uit-metabarcoding@users.noreply.github.com>
2021-01-14 19:42:56 +0100

    Update mjolnir6_FRIGGA.R

--- R/mjolnir6_FRIGGA.R
+++ R/mjolnir6_FRIGGA.R
@@ -13,22 +13,22 @@
 
 mjolnir6_FRIGGA <- function(lib=NULL,sept=";;"){
   # sept = separator characters used in taxonomy-annotated file and abundances file, respectively (default: ';;' )
-  message("Producing the combined file.")
+  message("FRYGGA will produce a combined file.")
 
   infile=paste0(lib,".ecotag.fasta.annotated.csv")
   abundances=paste0(lib,".SWARM_output.counts.csv")
   outfile=paste0(lib,".All_MOTUs.csv")
 
-  message("Reading ecotag-annotated database from THOR...")
+  message("FRYGGA is reading the ecotag-annotated database from THOR...")
   ecotag_db <- read.table(infile,sep=substr(sept,1,1),head=T,stringsAsFactors=F)
-  message("Ecotag database read including ", nrow(ecotag_db)," total MOTUs.")
+  message("FRYGGA has read the taxonomy database, with ", nrow(ecotag_db)," total MOTUs.")
   # Delete "None" from the taxonomy database
   ecotag_db[ecotag_db=="None"] <- ""
 
-  message("Reading abundances database from ODIN...")
+  message("FRYGGA is reading the abundances database from ODIN...")
   abun_db <- read.table(abundances,sep=substr(sept,2,2),head=T,stringsAsFactors=F)
   n_samples <- ncol(abun_db[,substr(names(abun_db),1,7)=="sample."])
-  message("Abundances database read including ", nrow(abun_db)," total MOTUs and ",n_samples," samples.")
+  message("FRYGGA has read the abundances database, including ", nrow(abun_db)," total MOTUs and ",n_samples," samples.")
 
   # Merge databases
   db <- merge(ecotag_db,abun_db,by="id")
@@ -46,7 +46,7 @@ mjolnir6_FRIGGA <- function(lib=NULL,sept=";;"){
   db <- db[,c(1:4,ncol(db)-1,ncol(db)-2,5:ncol(db)-3,ncol(db))]
 
   write.table(db,outfile,sep=";",quote=F,row.names=F)
-  message("File ", outfile, " written, including ",nrow(db)," MOTUs with ",sum(db$total_reads)," total reads in ",n_samples," samples.")
+  message("FRYGGA is done. File ", outfile, " written, including ",nrow(db)," MOTUs with ",sum(db$total_reads)," total reads in ",n_samples," samples.")
   message("(",nrow(db[db$total_reads>1,])," non-singletons MOTUs).")
 }
 
