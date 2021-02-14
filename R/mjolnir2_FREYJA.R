a9dafad50ab8e6f52ee1bc60a9f10ca26c8b5494
UiT Metabarcoding <70535159+uit-metabarcoding@users.noreply.github.com>
2021-02-15 00:34:12 +0100

    Update mjolnir2_FREYJA.R

--- R/mjolnir2_FREYJA.R
+++ R/mjolnir2_FREYJA.R
@@ -7,7 +7,8 @@ mjolnir2_FREYJA <- function(lib_prefix,cores,Lmin=299,Lmax=320){
   clust <- makeCluster(no_cores)
   X <- NULL
   for (i in 1:cores) for (j in 1:length(lib_prefix)) {
-    X <- c(X,paste0("illuminapairedend -r ",lib_prefix[j],"_R2_part_",sprintf("%02d",i),".fastq ",lib_prefix[j],"_R1_part_",sprintf("%02d",i),".fastq | obigrep -p \'score>40.00\' | ngsfilter -t ngsfilter_",lib_prefix[j],".tsv | obigrep -p \'seq_length>",Lmin,"\' -p \'seq_length<",Lmax,"\' -s \'^[ACGT]+$\' -p \'forward_tag!=None\' -p \'reverse_tag!=None\' --fasta-output > ",lib_prefix[j],".filtered_length_part_",sprintf("%02d",i),".fasta"))
+    if (cores<10) {formatted_i <- i} else {formatted_i <- sprintf("%02d",i)} 
+    X <- c(X,paste0("illuminapairedend -r ",lib_prefix[j],"_R2_part_",formatted_i,".fastq ",lib_prefix[j],"_R1_part_",formatted_i,".fastq | obigrep -p \'score>40.00\' | ngsfilter -t ngsfilter_",lib_prefix[j],".tsv | obigrep -p \'seq_length>",Lmin,"\' -p \'seq_length<",Lmax,"\' -s \'^[ACGT]+$\' -p \'forward_tag!=None\' -p \'reverse_tag!=None\' --fasta-output > ",lib_prefix[j],".filtered_length_part_",sprintf("%02d",i),".fasta"))
   }
   clusterExport(clust, "X",envir = environment())
   parLapply(clust,X, function(x) system(x,intern=T,wait=T))
