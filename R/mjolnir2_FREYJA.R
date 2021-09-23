8ad4151fcb90c282a035b171d7023c88c622f847
UiT Metabarcoding <70535159+uit-metabarcoding@users.noreply.github.com>
2021-09-23 17:45:07 +0100

    Update mjolnir2_FREYJA.R

--- R/mjolnir2_FREYJA.R
+++ R/mjolnir2_FREYJA.R
@@ -22,8 +22,9 @@ mjolnir2_FREYJA <- function(lib_prefix,cores,Lmin=299,Lmax=320,lib="",
       #if (cores<10) {formatted_i <- i} else {formatted_i <- sprintf("%02d",i)}
       formatted_i <- sprintf("%02d",i)
       X <- c(X,paste0("illuminapairedend -r ",lib_prefix[j],"_R2_part_",formatted_i,".fastq ",lib_prefix[j],"_R1_part_",formatted_i,".fastq | obigrep -p \'score>40.00\' | ngsfilter -t ngsfilter_",lib_prefix[j],".tsv | obigrep -p \'seq_length>",Lmin,"\' -p \'seq_length<",Lmax,"\' -s \'^[ACGT]+$\' -p \'forward_tag!=None\' -p \'reverse_tag!=None\' --fasta-output > ",lib_prefix[j],".filtered_length_part_",sprintf("%02d",i),".fasta"))
+    }  
     for (prefix in libs) libslist <- paste0(libslist,prefix,".filtered_length_part*.fasta ")
-    } else {
+  } else {
       metadata <- read.csv2(paste0(lib,"_metadata.csv"))
       fastqR1_list <- metadata$fastq_name_R1
       agnomens <-  metadata$mjolnir_agnomens
@@ -34,7 +35,7 @@ mjolnir2_FREYJA <- function(lib_prefix,cores,Lmin=299,Lmax=320,lib="",
          X <- c(X,paste0("illuminapairedend -r ",gsub(R1_motif,R2_motif,fastqR1_list[i]), " ",fastqR1_list[i]," | obigrep -p \'score>40.00\' | ngsfilter -t ngsfilter_",agnomens[i],".tsv | obigrep -p \'seq_length>",Lmin,"\' -p \'seq_length<",Lmax,"\' -s \'^[ACGT]+$\' -p \'forward_tag!=None\' -p \'reverse_tag!=None\' --fasta-output > ",agnomens[i],".filtered_length_part_",sprintf("%02d",i),".fasta"))
          libslist <- paste0(libslist,agnomens[i],".filtered_length_part_",sprintf("%02d",i),".fasta ")
       }
-    }
+  }
     clusterExport(clust, list("X","old_path","obipath"),envir = environment())
     clusterEvalQ(clust, {Sys.setenv(PATH = paste(old_path, obipath, sep = ":"))}) 
     parLapply(clust,X, function(x) system(x,intern=T,wait=T))
