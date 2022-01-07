cb95cd2cfe4c0a6cf17e9e06d906974be0e52ebc
UiT Metabarcoding <70535159+uit-metabarcoding@users.noreply.github.com>
2022-01-07 20:59:21 +0100

    Update mjolnir6_FRIGGA.R

--- R/mjolnir6_FRIGGA.R
+++ R/mjolnir6_FRIGGA.R
@@ -1,32 +1,31 @@
 # FRIGGA: Final Recount and Integration of Generated Genealogies and Abundances
 
-## Script for combining an abundance CSV file from ODIN with a taxonomy-annotated CSV file from THOR.
+## Script for combining an abundance TSV file from ODIN with a taxonomy-annotated TSV file from THOR.
 ## The script will have the name of the library (typically 4 characters, e.g. LIBR) as input.
-## The taxonomy file must be called LIBR.ecotag.fasta.annotated.csv
-## The abundance file must be called LIBR.SWARM_output.counts.csv
-## The separator characters for both the taxonomy and abundance CSV files can be also used as input.
-## By default, they are both are semicolons ";;".
+## The taxonomy file must be called LIBR.ecotag.fasta.annotated.tsv
+## The abundance file must be called LIBR.SWARM_output.counts.tsv
+## The separator characters for both the taxonomy and abundance tsv files must be tabs.
 ## The abundances file must include sample columns names starting with "sample."
-## The output file is a CSV file called LIBR.All_MOTUs.csv
+## The output file is a TSV file called LIBR.All_MOTUs.tsv
 ## FRIGGA deprecates the owi_combine function from previous pipelines (Project Metabarpark, 2016).
 ## By Owen S. Wangensteen 
 
-mjolnir6_FRIGGA <- function(lib=NULL,sept=";;"){
+mjolnir6_FRIGGA <- function(lib=NULL){
   # sept = separator characters used in taxonomy-annotated file and abundances file, respectively (default: ';;' )
   message("FRYGGA will produce a combined file.")
 
-  infile=paste0(lib,".ecotag.fasta.annotated.csv")
-  abundances=paste0(lib,".SWARM_output.counts.csv")
-  outfile=paste0(lib,".All_MOTUs.csv")
+  infile=paste0(lib,".ecotag.fasta.annotated.tsv")
+  abundances=paste0(lib,".SWARM_output.counts.tsv")
+  outfile=paste0(lib,".All_MOTUs.tsv")
 
   message("FRYGGA is reading the ecotag-annotated database from THOR...")
-  ecotag_db <- read.table(infile,sep=substr(sept,1,1),head=T,stringsAsFactors=F)
+  ecotag_db <- read.table(infile,sep="\t",head=T,stringsAsFactors=F)
   message("FRYGGA has read the taxonomy database, with ", nrow(ecotag_db)," total MOTUs.")
   # Delete "None" from the taxonomy database
   ecotag_db[ecotag_db=="None"] <- ""
 
   message("FRYGGA is reading the abundances database from ODIN...")
-  abun_db <- read.table(abundances,sep=substr(sept,2,2),head=T,stringsAsFactors=F)
+  abun_db <- read.table(abundances,sep="\t",head=T,stringsAsFactors=F)
   n_samples <- ncol(abun_db[,substr(names(abun_db),1,7)=="sample."])
   message("FRYGGA has read the abundances database, including ", nrow(abun_db)," total MOTUs and ",n_samples," samples.")
 
@@ -45,7 +44,7 @@ mjolnir6_FRIGGA <- function(lib=NULL,sept=";;"){
   # Reorder total_counts and cluster weight columns
   db <- db[,c(1:4,ncol(db)-1,ncol(db)-2,5:ncol(db)-3,ncol(db))]
 
-  write.table(db,outfile,sep=";",quote=F,row.names=F)
+  write.table(db,outfile,sep="\t",quote=F,row.names=F)
   message("FRYGGA is done. File ", outfile, " written, including ",nrow(db)," MOTUs with ",sum(db$total_reads)," total reads in ",n_samples," samples.")
   message("(",nrow(db[db$total_reads>1,])," non-singletons MOTUs).")
 }
