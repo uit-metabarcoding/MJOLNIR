a531675ba6a4326baf8f9413ddfd245d1ceb562d
UiT Metabarcoding <70535159+uit-metabarcoding@users.noreply.github.com>
2022-01-07 21:01:45 +0100

    Update mjolnir7_LOKI.R

--- R/mjolnir7_LOKI.R
+++ R/mjolnir7_LOKI.R
@@ -1,18 +1,18 @@
 # LOKI: LULU Overseeing with Kinship Identification 
 
 # This function is a convenient wrapper of LULU for the MJOLNIR metabarcoding pipeline.
-# It starts from the combined dataset of abundances and taxonomy from the previous step (FRIGGA): LIBR.All_MOTUs.csv
+# It starts from the combined dataset of abundances and taxonomy from the previous step (FRIGGA): LIBR.All_MOTUs.tsv
 # Then a match list of representative MOTU sequences is created using VSEARCH and saved as a txt file.
 # Then MOTUs which are potential pseudogenes are labelled and removed using LULU.
 # The output includes the LIBR.match_list.txt file and 3 CSV files:
-# - A final curated metabarcoding dataset: LIBR.Curated_LULU.csv
-# - A dataset of discarded MOTUs: LIBR.Discarded_LULU.csv
-# - A file with informaton on the fate of discarded MOTUs (with IDs of putative mother sequences): LIBR.Deleted_LULU_fate.csv  
+# - A final curated metabarcoding dataset: LIBR.Curated_LULU.tsv
+# - A dataset of discarded MOTUs: LIBR.Discarded_LULU.tsv
+# - A file with informaton on the fate of discarded MOTUs (with IDs of putative mother sequences): LIBR.Deleted_LULU_fate.tsv  
 
 mjolnir7_LOKI <- function(lib,min_id = .84){
 
   # lib is the name of the library to be processed. Usually a four-character uppercase name.
-  # Input file name must be in the format: LIBR.All_MOTUs.csv. Then lib must be = "LIBR"
+  # Input file name must be in the format: LIBR.All_MOTUs.tsv. Then lib must be = "LIBR"
   # min_id is the minimum identity between two sequences to be kept in the match_list output. Default: 0.84 
   
   message("LOKI will produce a pairwise match list for LULU.")
@@ -22,7 +22,7 @@ mjolnir7_LOKI <- function(lib,min_id = .84){
   suppressPackageStartupMessages(library(lulu))
   
   #Load the dataset
-  db <- read.csv(paste0(lib,".All_MOTUs.csv"),sep=";",head=T,stringsAsFactors = F)
+  db <- read.table(paste0(lib,".All_MOTUs.tsv"),sep="\t",head=T,stringsAsFactors = F)
     # Select sample abundance columns
   sample_cols <- (1:ncol(db))[tolower(substr(names(db),6,11))=="sample"]
   otutable_name <-db[,sample_cols]
@@ -36,14 +36,14 @@ mjolnir7_LOKI <- function(lib,min_id = .84){
   
   #Get discarded table:
   discarded_db <- db[db$id %in% curated_result$discarded_otus,]
-  write.table(discarded_db,paste0(lib,".Discarded_LULU.csv"),row.names = F,sep=";",quote = F)
+  write.table(discarded_db,paste0(lib,".Discarded_LULU.tsv"),row.names = F,sep="\t",quote = F)
 
   #Get curated table:
   curated_db <- db[db$id %in% curated_result$curated_otus,]
   curated_db <- curated_db[order(curated_db$id),]
   curated_db[,sample_cols] <- curated_result$curated_table
   curated_db$total_reads <- rowSums(curated_result$curated_table)
-  write.table(curated_db,paste0(lib,".Curated_LULU.csv"),row.names = F,sep=";",quote = F)
+  write.table(curated_db,paste0(lib,".Curated_LULU.tsv"),row.names = F,sep="\t",quote = F)
   
   #Get fate of deleted taxa 
   deleted_otu_fate <- (curated_result$otu_map[curated_result$otu_map$curated=="merged",])
@@ -60,9 +60,9 @@ mjolnir7_LOKI <- function(lib,min_id = .84){
     deleted_otu_fate$order[i] <- db$order[db$id==deleted_otu_fate$parent_id[i]]
     deleted_otu_fate$family[i] <- db$family[db$id==deleted_otu_fate$parent_id[i]]
   }
-  write.table(deleted_otu_fate,paste0(lib,".Deleted_LULU_fate.csv"),row.names = F,sep=";",quote = F)
+  write.table(deleted_otu_fate,paste0(lib,".Deleted_LULU_fate.tsv"),row.names = F,sep="\t",quote = F)
 
-  message("LOKI is done. He kept ",nrow(curated_db)," MOTUs in the curated database, which He stored in file: ",paste0(lib,".Curated_LULU.csv"))
-  message("LOKI discarded ",nrow(discarded_db)," MOTUs, which He stored in file: ",paste0(lib,".Discarded_LULU.csv"))
-  message("LOKI stored the fate of the discarded MOTUs in file: ",paste0(lib,".Deleted_LULU_fate.csv"))
+  message("LOKI is done. He kept ",nrow(curated_db)," MOTUs in the curated database, which He stored in file: ",paste0(lib,".Curated_LULU.tsv"))
+  message("LOKI discarded ",nrow(discarded_db)," MOTUs, which He stored in file: ",paste0(lib,".Discarded_LULU.tsv"))
+  message("LOKI stored the fate of the discarded MOTUs in file: ",paste0(lib,".Deleted_LULU_fate.tsv"))
 }
