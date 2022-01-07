6bb46425f3764fb1c155fe214ec2f13e7d7833fb
UiT Metabarcoding <70535159+uit-metabarcoding@users.noreply.github.com>
2022-01-07 21:36:45 +0100

    Update mjolnir7_LOKI.R

--- R/mjolnir7_LOKI.R
+++ R/mjolnir7_LOKI.R
@@ -16,34 +16,34 @@ mjolnir7_LOKI <- function(lib,min_id = .84){
   # min_id is the minimum identity between two sequences to be kept in the match_list output. Default: 0.84 
   
   message("LOKI will produce a pairwise match list for LULU.")
-  system(paste0("vsearch --usearch_global ",lib,".seeds_nonsingleton.fasta --db ",lib,".seeds_nonsingleton.fasta --self --id ",min_id," --iddef 1 --userout ",lib,".match_list.txt -userfields query+target+id --maxaccepts 0 --query_cov .9 --maxhits 10"),intern=T,wait=T)
+  system(paste0("vsearch --usearch_global ",lib,"_seeds_nonsingleton.fasta --db ",lib,"_seeds_nonsingleton.fasta --self --id ",min_id," --iddef 1 --userout ",lib,"_match_list.txt -userfields query+target+id --maxaccepts 0 --query_cov .9 --maxhits 10"),intern=T,wait=T)
   message("LOKI will now remove the pseudogenes with LULU.")
   
   suppressPackageStartupMessages(library(lulu))
   
   #Load the dataset
-  db <- read.table(paste0(lib,".All_MOTUs.tsv"),sep="\t",head=T,stringsAsFactors = F)
+  db <- read.table(paste0(lib,"_All_MOTUs.tsv"),sep="\t",head=T,stringsAsFactors = F)
     # Select sample abundance columns
   sample_cols <- (1:ncol(db))[tolower(substr(names(db),6,11))=="sample"]
   otutable_name <-db[,sample_cols]
   rownames(otutable_name) <- db$id
   
   #Load the matchlist
-  matchlist_name <- read.csv(paste0(lib,".match_list.txt"),sep="\t",head=F,stringsAsFactors = F)
+  matchlist_name <- read.csv(paste0(lib,"_match_list.txt"),sep="\t",head=F,stringsAsFactors = F)
   
   #Run LULU
   curated_result <- lulu(otutable_name, matchlist_name)
   
   #Get discarded table:
   discarded_db <- db[db$id %in% curated_result$discarded_otus,]
-  write.table(discarded_db,paste0(lib,".Discarded_LULU.tsv"),row.names = F,sep="\t",quote = F)
+  write.table(discarded_db,paste0(lib,"_Discarded_LULU.tsv"),row.names = F,sep="\t",quote = F)
 
   #Get curated table:
   curated_db <- db[db$id %in% curated_result$curated_otus,]
   curated_db <- curated_db[order(curated_db$id),]
   curated_db[,sample_cols] <- curated_result$curated_table
   curated_db$total_reads <- rowSums(curated_result$curated_table)
-  write.table(curated_db,paste0(lib,".Curated_LULU.tsv"),row.names = F,sep="\t",quote = F)
+  write.table(curated_db,paste0(lib,"_Curated_LULU.tsv"),row.names = F,sep="\t",quote = F)
   
   #Get fate of deleted taxa 
   deleted_otu_fate <- (curated_result$otu_map[curated_result$otu_map$curated=="merged",])
@@ -60,9 +60,9 @@ mjolnir7_LOKI <- function(lib,min_id = .84){
     deleted_otu_fate$order[i] <- db$order[db$id==deleted_otu_fate$parent_id[i]]
     deleted_otu_fate$family[i] <- db$family[db$id==deleted_otu_fate$parent_id[i]]
   }
-  write.table(deleted_otu_fate,paste0(lib,".Deleted_LULU_fate.tsv"),row.names = F,sep="\t",quote = F)
+  write.table(deleted_otu_fate,paste0(lib,"_Deleted_LULU_fate.tsv"),row.names = F,sep="\t",quote = F)
 
-  message("LOKI is done. He kept ",nrow(curated_db)," MOTUs in the curated database, which He stored in file: ",paste0(lib,".Curated_LULU.tsv"))
-  message("LOKI discarded ",nrow(discarded_db)," MOTUs, which He stored in file: ",paste0(lib,".Discarded_LULU.tsv"))
-  message("LOKI stored the fate of the discarded MOTUs in file: ",paste0(lib,".Deleted_LULU_fate.tsv"))
+  message("LOKI is done. He kept ",nrow(curated_db)," MOTUs in the curated database, which He stored in file: ",paste0(lib,"_Curated_LULU.tsv"))
+  message("LOKI discarded ",nrow(discarded_db)," MOTUs, which He stored in file: ",paste0(lib,"_Discarded_LULU.tsv"))
+  message("LOKI stored the fate of the discarded MOTUs in file: ",paste0(lib,"_Deleted_LULU_fate.tsv"))
 }
