a238319e3af79e8226afa154b8955c6c943735c7
UiT Metabarcoding <70535159+uit-metabarcoding@users.noreply.github.com>
2022-01-22 17:04:08 +0100

    Update mjolnir2_FREYJA.R

--- R/mjolnir2_FREYJA.R
+++ R/mjolnir2_FREYJA.R
@@ -8,7 +8,7 @@
 # When demultiplexed=TRUE, you must also specify the primer_F and primer_R sequences in the options input to FREYJA. COI Leray-XT primers are specified by default.
 # Otherwise, when demultiplexed=FALSE, the primers information must be already written in the LIBX_ngsfilter.tsv files.
 
-mjolnir2_FREYJA <- function(lib_prefix="",cores=1,Lmin=299,Lmax=320,lib="",
+mjolnir2_FREYJA <- function(lib_prefix="",cores=1,Lmin=299,Lmax=320,lib="", fasta_output=T,
                             demultiplexed=F,primer_F="GGWACWRGWTGRACWNTNTAYCCYCC",primer_R="TANACYTCNGGRTGNCCRAARAAYCA",R1_motif="_R1",R2_motif="_R2",obipath=""){
   message("FREYJA will do paired-end alignment, demultiplexing and length filter.")  
   suppressPackageStartupMessages(library(parallel))
@@ -18,14 +18,17 @@ mjolnir2_FREYJA <- function(lib_prefix="",cores=1,Lmin=299,Lmax=320,lib="",
   clust <- makeCluster(no_cores)
   X <- NULL
   libslist <- NULL
-
+  ext_file <- ifelse(fasta_output,".fasta",".fastq")
+  fasta_modifier <- ifelse(fasta_output," --fasta-output","")
   if (!demultiplexed){
     for (i in 1:cores) for (j in 1:length(lib_prefix)) {
       #if (cores<10) {formatted_i <- i} else {formatted_i <- sprintf("%02d",i)}
       formatted_i <- sprintf("%02d",i)
-      X <- c(X,paste0("illuminapairedend -r ",lib_prefix[j],"_R2_part_",formatted_i,".fastq ",lib_prefix[j],"_R1_part_",formatted_i,".fastq | obigrep -p \'score>40.00\' | ngsfilter -t ngsfilter_",lib_prefix[j],".tsv | obigrep -p \'seq_length>",Lmin,"\' -p \'seq_length<",Lmax,"\' -s \'^[ACGT]+$\' -p \'forward_tag!=None\' -p \'reverse_tag!=None\' --fasta-output > ",lib_prefix[j],"_filtered_length_part_",sprintf("%02d",i),".fasta"))
-    }  
-    for (prefix in lib_prefix) libslist <- paste0(libslist,prefix,"_filtered_length_part*.fasta ")
+      X <- c(X,paste0("illuminapairedend -r ",lib_prefix[j],"_R2_part_",formatted_i,".fastq ",lib_prefix[j],"_R1_part_",formatted_i,
+                                        ".fastq | obigrep -p \'score>40.00\' | ngsfilter -t ngsfilter_",lib_prefix[j],".tsv | obigrep -p \'seq_length>",
+                                        Lmin,"\' -p \'seq_length<",Lmax,"\' -s \'^[ACGT]+$\' -p \'forward_tag!=None\' -p \'reverse_tag!=None\'",fasta_modifier," > ",
+                                        lib_prefix[j],"_filtered_length_part_",sprintf("%02d",i),ext_file)) 
+    for (prefix in lib_prefix) libslist <- paste0(libslist,prefix,"_filtered_length_part*",ext_file," ")
   } else {
       metadata <- read.table(paste0(lib,"_metadata.tsv"),sep="\t",header=T)
       fastqR1_list <- metadata$fastq_name_R1
@@ -45,10 +48,10 @@ mjolnir2_FREYJA <- function(lib_prefix="",cores=1,Lmin=299,Lmax=320,lib="",
     # If not demultiplexed, then join all parts into a joined file and then split it into samples
     if (!demultiplexed){
       message("FREYJA is joining filtered reads into a single file.")
-      system(paste0("cat ",libslist," > ",lib,"_joined.fasta"),intern=T,wait=T)
-      message("File ",lib,".joined.fasta written.")
+      system(paste0("cat ",libslist," > ",lib,"_joined",ext_file),intern=T,wait=T)
+      message("File ",lib,".joined",ext_file," written.")
       message("HELA will create individual files for each sample.")
-      system(paste0("obisplit -t sample ",lib,"_joined.fasta"),intern=T,wait=T)
+      system(paste0("obisplit -t sample ",lib,"_joined",ext_file),intern=T,wait=T)
     }
     message("FREYJA is done.")
 }
