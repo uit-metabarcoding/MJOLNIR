5d944291545810f4cf870e53d5f38def83e5dc85
UiT Metabarcoding <70535159+uit-metabarcoding@users.noreply.github.com>
2022-01-26 16:03:13 +0100

    Update mjolnir3_HELA.R

--- R/mjolnir3_HELA.R
+++ R/mjolnir3_HELA.R
@@ -7,7 +7,7 @@
 # This is possibly a good idea for very large datasets (with > 5 million unique sequences before clustering)
 # The final dataset output is in VSEARCH format, so it can be directly fed into SWARM (ODIN).
 
-mjolnir3_HELA <- function(lib, cores, remove_singletons = T, obipath = ""){
+mjolnir3_HELA <- function(lib, cores, remove_singletons = TRUE, obipath = ""){
   old_path <- Sys.getenv("PATH")
   Sys.setenv(PATH = paste(old_path, obipath, sep = ":"))
   fastq_list <- list.files(pattern="^[a-zA-Z0-9]{4}_sample_[a-zA-Z0-9]{3}.fastq$")
